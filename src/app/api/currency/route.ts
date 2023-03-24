082f7b717786ce50e501ba5f533994019a1399b8
Krachkov Anton <a.krachkov@qiwi.com>
2023-03-24 14:15:22 +0400

    fix: fix currency rate getter

--- src/app/api/currency/route.ts
+++ src/app/api/currency/route.ts
@@ -1,8 +1,16 @@
 import { NextResponse } from 'next/server'
-import { getCurrencyRate } from '@shared/util/currency'
-import { Currency } from '@enums/currency'
 
-export async function GET() {
-  const rate = getCurrencyRate(Currency.USD)
-  return NextResponse.json(rate)
+export async function GET(res: Response) {
+  const response = await fetch(
+    'https://www.random.org/integers/?num=1&min=50&max=80&col=1&base=10&format=plain&rnd=new',
+    { cache: 'no-store' } // Revalidate every 10 seconds
+  )
+
+  if (response.ok) {
+    const json = await response.json()
+
+    return NextResponse.json(json, {
+      headers: { 'Cache-Control': 'no-store max-age=0' },
+    })
+  }
 }
