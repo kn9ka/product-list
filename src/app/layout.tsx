6a15784880263ecf65cb76cdb0b3c2e0248a6be8
Krachkov Anton <a.krachkov@qiwi.com>
2023-03-24 18:36:05 +0400

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
-import './globals.css';
+import { Analytics } from '@vercel/analytics/react'
+import './globals.css'
 
 export const metadata = {
   title: 'Product List',
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
-  );
+  )
 }
